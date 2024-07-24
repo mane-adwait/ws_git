07616a2ba978cc6482647796c145a14839d39e85
Adwait Mane <mane.learn.2023.04@gmail.com>
2024-07-24 16:48:31 -0400

    Backup. New file pend0.py.

---
+++ pend0.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Parameters
+g = 9.81    # acceleration due to gravity (m/s^2)
+L = 1.0     # length of pendulum (m)
+theta_0 = np.pi/4  # initial angle (rad)
+omega_0 = 0.0      # initial angular velocity (rad/s)
+dt = 0.01         # time step (s)
+t_final = 10.0     # final time (s)
+
+# Initialize arrays for time, angle, and angular velocity
+
+# Initial conditions
+
+# Euler's method
+
+# Plot angle and angular velocity as functions of time
