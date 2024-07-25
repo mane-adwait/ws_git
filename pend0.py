f5144ac43940211c53342c3425c7cac7658418f6
Adwait Mane <mane.learn.2023.04@gmail.com>
2024-07-25 16:42:51 -0400

    Minor change.

--- pend0.py
+++ pend0.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 g = 9.81    # acceleration due to gravity (m/s^2)
 m = 1.0     # mass of pendulum (kg)
 L = 1.0     # length of pendulum (m)
-c = 0.1     # damping coefficient (s^-1)
+c = 0.5     # damping coefficient (s^-1)
 theta_0 = np.pi/8  # initial angle (rad)
 omega_0 = 0.0      # initial angular velocity (rad/s)
 dt = 0.01         # time step (s)
@@ -20,7 +20,7 @@ omega = np.zeros(t_array.size)
 theta[0] = theta_0
 omega[0] = omega_0
 
-# Euler's method for undamped pendulum
+# Euler's method for damped pendulum
 for k in range(0, t_array.size-1):
     theta[k+1] = theta[k] + dt*omega[k]
     omega[k+1] = omega[k] + dt*(-g/L*np.sin(theta[k])  - c/(m*L**2)*omega[k] )
