77ce1cb4d0bd124752238a4f4a3b51bc6d6c431e
Adwait Mane <mane.learn.2023.04@gmail.com>
2024-07-25 12:21:48 -0400

    Backup.

--- pend0.py
+++ pend0.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 # Parameters
 g = 9.81    # acceleration due to gravity (m/s^2)
+m = 1.0     # mass of pendulum (kg)
 L = 1.0     # length of pendulum (m)
 c = 0.5     # damping coefficient (s^-1)
 theta_0 = np.pi/4  # initial angle (rad)
@@ -22,7 +23,15 @@ omega[0] = omega_0
 # Euler's method
 for k in range(0, t_array.size-1):
     theta[k+1] = theta[k] + dt*omega[k]
-    omega[k+1] = omega[k] + dt*(-g/L*np.sin(theta[k]) - c/())
-    
+    omega[k+1] = omega[k] + dt*(-g/L*np.sin(theta[k]) - c/(m*L**2)*omega[k])
 
 # Plot angle and angular velocity as functions of time
+plt.figure(figsize=(10,5))
+plt.plot(t_array, theta, label='Angle')
+plt.plot(t_array, omega, label='Angular velocity')
+plt.xlabel('Time (s)')
+plt.ylabel('Angle, Angular velocity')
+plt.title('Damped pendulum')
+plt.legend()
+plt.grid()
+plt.show()
