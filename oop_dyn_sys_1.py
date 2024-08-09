f1e75bdf53110ac37bc258e31deef8466f523905
Adwait Mane <mane.learn.2023.04@gmail.com>
2024-08-09 19:52:08 -0400

    Class.

--- oop_dyn_sys_1.py
+++ oop_dyn_sys_1.py
@@ -1,3 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+class DynamicalSystem:
+    # ----- Member functions -----
+    def __init__(self, func):
+        # ----- Member variables -----
+        self.dt = 0.001 # Time step.
+        self.ode = func
+
+    def simulate(self, tf, x0):
+        t = np.arange(0,tf,self.dt) # Initialize time vector.
+
+        # Run the ode function (called func with a dummy input)
+        dummy_output = self.ode(np.ones((100,1)))
+        sx = dummy_output.shape
